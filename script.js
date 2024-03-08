e8b43cce4a401e8f924bfa7070bba32eeeeee90f
Christian Kodehode <kodehodechristian@gmail.com>
2024-03-08 15:50:20 +0100

    Finished styling for section 2

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Select the background container
   const backgroundContainer = document.querySelector(
-    "#background-image-container"
+    ".background-image-container"
   );
-
-  // Select all links in the navigation
   const navLinks = document.querySelectorAll(".nav-links a");
 
-  // Add click event listeners to all navigation links
   navLinks.forEach((link) => {
     link.addEventListener("click", function (event) {
-      // Check if the clicked link's href attribute is '#section1'
       if (event.currentTarget.getAttribute("href") === "#section1") {
-        // If yes, set the background opacity to 1
+        // Remove the darkened overlay and blur when '#section1' is clicked
+        backgroundContainer.classList.remove("darkened");
         backgroundContainer.style.opacity = "1";
-        backgroundContainer.style.filter = "blur(0px)"; // Remove blur
+        backgroundContainer.style.filter = "none"; // Clear any filters
       } else {
-        // For all other links, change the background opacity and add blur
-        backgroundContainer.style.opacity = "0.5";
-        backgroundContainer.style.filter = "blur(5px)";
+        // Add the darkened overlay and blur for other links
+        backgroundContainer.classList.add("darkened");
+        backgroundContainer.style.filter = "blur(10px)";
+        backgroundContainer.style.background = `black, url("images/used/background-img-FINAL.png") 1%`;
       }
     });
   });

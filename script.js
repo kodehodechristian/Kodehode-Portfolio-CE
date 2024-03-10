6af6b9eb27729a9da1176ab6044e5b7b17d5b66e
Christian Kodehode <kodehodechristian@gmail.com>
2024-03-11 00:18:49 +0100

    Finished page 3 & added overlay + responsive

--- script.js
+++ script.js
@@ -4,6 +4,38 @@ document.addEventListener("DOMContentLoaded", function () {
   );
   const navLinks = document.querySelectorAll(".nav-links a");
   const sections = document.querySelectorAll("section");
+  const overlayTexts = {
+    "problem-solving":
+      "My ability to solve whatever problem gets thrown at me.",
+    chatgpt: "Using and utilising the vast hive-mind knowledge of AI",
+    css: "I use CSS to apply styling to my projects.",
+    react: "Used to streamline the process of creating web-apps.",
+    python: "Used for creating ease-of-life scripts.",
+    figma:
+      "I use Figma to give myself an idea of how my project should look like.",
+    html: "I use HTML to give structure to my web-creations.",
+    javascript:
+      "I am using Javascript to make my websites/projects interactive.",
+  };
+
+  // Overlay functionality
+  const gridItems = document.querySelectorAll(
+    "#grid-container2 > div:not(.overlay)"
+  );
+  const overlay = document.getElementById("overlay");
+
+  gridItems.forEach((item) => {
+    item.addEventListener("mouseenter", function () {
+      const itemId = this.id; // Get the id of the hovered item
+      const textToShow = overlayTexts[itemId]; // Look up the text for this id
+      overlay.innerText = textToShow || "Details not available"; // Set the text or a default message
+      overlay.style.display = "block"; // Show the overlay
+    });
+
+    item.addEventListener("mouseleave", function () {
+      overlay.style.display = "none"; // Hide the overlay
+    });
+  });
 
   // Function to handle navigation link clicks
   navLinks.forEach((link) => {

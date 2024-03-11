0c485df935ee5eb5230cb2cb193d1368deeda0e5
Christian Kodehode <kodehodechristian@gmail.com>
2024-03-11 06:04:41 +0100

    Finishing assignment

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
       "I am using Javascript to make my websites/projects interactive.",
   };
 
-  // Overlay functionality
+  // Overlay functionality section 3
   const gridItems = document.querySelectorAll(
     "#grid-container2 > div:not(.overlay)"
   );
@@ -27,14 +27,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
   gridItems.forEach((item) => {
     item.addEventListener("mouseenter", function () {
-      const itemId = this.id; // Get the id of the hovered item
-      const textToShow = overlayTexts[itemId]; // Look up the text for this id
-      overlay.innerText = textToShow || "Details not available"; // Set the text or a default message
-      overlay.style.display = "block"; // Show the overlay
+      const itemId = this.id;
+      const textToShow = overlayTexts[itemId];
+      overlay.innerText = textToShow || "Details not available";
+      overlay.style.display = "block";
     });
 
     item.addEventListener("mouseleave", function () {
-      overlay.style.display = "none"; // Hide the overlay
+      overlay.style.display = "none";
     });
   });
 
@@ -44,7 +44,7 @@ document.addEventListener("DOMContentLoaded", function () {
       if (event.currentTarget.getAttribute("href") === "#section1") {
         backgroundContainer.classList.remove("darkened");
         backgroundContainer.style.opacity = "1";
-        backgroundContainer.style.filter = "none"; // Clear any filters
+        backgroundContainer.style.filter = "none";
       } else {
         backgroundContainer.classList.add("darkened");
         backgroundContainer.style.filter = "blur(10px)";
@@ -78,13 +78,48 @@ document.addEventListener("DOMContentLoaded", function () {
 
 const gitLogo = document.getElementById("git-logo");
 const cvPdfLogo = document.getElementById("cv-pdf-logo");
+const project1 = document.getElementById("project1");
+const project2 = document.getElementById("project2");
+const project3 = document.getElementById("project3");
+const project4 = document.getElementById("project4");
 
-// GitHub Page URL
+// GitHub
 gitLogo.addEventListener("click", function () {
   window.open("https://github.com/kodehodechristian", "_blank");
 });
 
-// Direct link to your CV PDF on GitHub
+// CV PDF on GitHub
 cvPdfLogo.addEventListener("click", function () {
-  window.open("https://github.com/yourusername/yourcv.pdf", "_blank");
+  window.open(
+    "https://github.com/kodehodechristian/Kodehode-Portfolio-CE/blob/main/CV-Christian.Ernst.pdf",
+    "_blank"
+  );
+});
+// Project1
+project1.addEventListener("click", function () {
+  window.open(
+    "https://kodehode-stavanger.github.io/user-data-assignment-kodehodechristian/",
+    "_blank"
+  );
+});
+// Project2
+project2.addEventListener("click", function () {
+  window.open(
+    "https://kodehode-stavanger.github.io/javascript-advanced-project-api-kodehodechristian/",
+    "_blank"
+  );
+});
+// Project3
+project3.addEventListener("click", function () {
+  window.open(
+    "https://kodehode-stavanger.github.io/javascript-advanced-project-drum-machine-kodehodechristian/",
+    "_blank"
+  );
+});
+// Project4
+project4.addEventListener("click", function () {
+  window.open(
+    "https://kodehode-stavanger.github.io/javascript-basic-mini-project-kodehodechristian/",
+    "_blank"
+  );
 });
